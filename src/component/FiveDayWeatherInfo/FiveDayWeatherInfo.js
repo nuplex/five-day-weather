8cd70ba5eb8f715f13678b6d4f35bcbd7df2a4e3
Chijioke Okonkwo <chokon@umd.edu>
2018-10-28 14:31:29 -0400

    handle when styling if no city/country (antartica?, ocean?)

--- src/component/FiveDayWeatherInfo/FiveDayWeatherInfo.js
+++ src/component/FiveDayWeatherInfo/FiveDayWeatherInfo.js
@@ -2,13 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {Grid, Row, Col} from "react-flexbox-grid";
 import WeatherInfo from "../WeatherInfo/WeatherInfo";
+import './FiveDayWeatherInfo.css';
 
 /**
  * Holds the five combined WeatherInfo's to display a forecast.
  */
 function FiveDayWeatherInfo({weatherData, degConvert}){
         return (
-            <div>
+            <div className="fdwi-cont">
                 {weatherData && weatherData.days &&
                     <Grid fluid>
                         <Row xs={12} center="xs">
