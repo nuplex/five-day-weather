1cf14d3ea2f101809da731a2e4b7b6fe78c6912d
Chijioke Okonkwo <chokon@umd.edu>
2020-06-12 15:36:24 -0400

    change listen

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const path = require('path');
 const app = express();
 
 const port = process.env.PORT || 8080;
+const env = process.env.NODE_ENV;
+const listen = env === 'production' ? `0.0.0.0:8080`:port;
 
 //routes is where the api calls will exist
 app.use(require('./routes'));
@@ -13,6 +15,6 @@ app.use('/', express.static(path.join(__dirname, '/build')));
 app.use('/*', express.static(path.join(__dirname, '/build')));
 
 //start the server
-app.listen(port, () => {
+app.listen(listen, () => {
     console.log(`Server is listening on port ${port}`);
 });
